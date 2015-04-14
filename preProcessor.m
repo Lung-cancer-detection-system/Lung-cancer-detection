524e71d6cd915a8dfccb1f1190c5b094e1a654b5
Yan <heyan4869@gmail.com>
2015-04-14 00:06:28 -0400

    test with gabor filter

--- preProcessor.m
+++ preProcessor.m
@@ -2,7 +2,7 @@ function greyimg = preProcessor()
 
 %% read the image file and preprocess
 % 032 is healthy
-path = 'resources/000032.dcm';
+path = 'resources/000008.dcm';
 
 % read the dcm image file
 img = dicomread(path);
@@ -16,5 +16,29 @@ greyimg = im2uint8(img);
 
 % calculate the average grey level
 avgimg = mean(double(reshape(greyimg, [], 1)));
+
+% %% use gabor filter to process the ct image
+% % set the parameters
+% lambda  = 8;
+% theta   = 0;
+% psi     = [0 pi/2];
+% gamma   = 0.5;
+% bw      = 1;
+% N       = 8;
+% img_in = greyimg;
+% % img_in(:,:,2:3) = [];   % discard redundant channels, it's gray anyway
+% img_out = zeros(size(img_in,1), size(img_in,2), N);
+% for n=1:N
+%     gb = gaborfilter(bw,gamma,psi(1),lambda,theta)...
+%         + 1i * gaborfilter(bw,gamma,psi(2),lambda,theta);
+%     % gb is the n-th gabor filter
+%     img_out(:,:,n) = imfilter(img_in, gb, 'symmetric');
+%     % filter output to the n-th channel
+%     theta = theta + 2*pi/N;
+%     % next orientation
+% end
+% img_out_disp = sum(abs(img_out).^2, 3).^0.5;
+% % default superposition method, L2-norm
+% img_out_disp = img_out_disp./max(img_out_disp(:));
 
 end
